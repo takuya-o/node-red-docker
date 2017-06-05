846239448caa029b2e819a5e7dcd3caba6e26c5b
Takuya Ono <takuya-o@users.sourceforge.net>
2017-06-05 19:58:18 +0900

    letsencryptの証明書の消し方

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -7,4 +7,4 @@
 
 sudo certbot certonly -v -t -m takuya@page.on-o.com -d node-red.docker.on-o.com -d node-red.on-o.com --apache
 
-#消すときには sudo certbot -v で、一覧から選択して消す (使っていたならその前にrevokeした方が良いかも)
+#消すときには sudo certbot delete -v で、一覧から選択して消す (使っていたならその前にrevokeした方が良いかも)
