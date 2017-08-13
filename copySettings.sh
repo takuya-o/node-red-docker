28ec9c3a15a2da1da87451ba8f1be55d8913e361
Takuya Ono <takuya-o@users.sourceforge.net>
2017-08-14 07:59:57 +0900

    docker iamge idのとり方をdocker-composeにする

--- copySettings.sh
+++ copySettings.sh
@@ -4,7 +4,7 @@
 # data/settings.jsなどをコピー
 
 #Dockerfileで作成した settings.js 取得
-ID=`docker run -it -d nodered_node-red sh`
+ID=`docker-compose ps -q`
 docker cp $ID:/data .
 docker cp $ID:/usr/src/node-red/node_modules/node-red/settings.js ./data/
 docker stop $ID
