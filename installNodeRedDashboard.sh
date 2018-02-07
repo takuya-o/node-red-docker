87de72e347bd3e42b525a2fea499f98618d98088
Takuya Ono <takuya-o@users.sourceforge.net>
2018-02-07 17:24:01 +0900

    node-red-contrib-facebookについては一度忘れる

--- installNodeRedDashboard.sh
+++ installNodeRedDashboard.sh
@@ -5,14 +5,8 @@
 # ついでにURL短縮 goo.glもインストールするようにした
 #
 
-#node-red-contrib-facebookにpythonが必要?
-
-#docker-compose exec --user root node-red sh -c "apk add --no-cache python make g++"
 docker-compose exec node-red \
 	       sh -c "cd /data;\
                       npm install node-red-dashboard &&\
                       npm install node-red-contrib-shorturl" &&\
 docker-compose restart node-red
-
-#うまく動かない
-#	npm install node-red-contrib-facebook
